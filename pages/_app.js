51811bd9ce8036dcf29e93b9151af4f844d481cb
ismail5g <ismail27.dec@gmail.com>
2021-02-04 10:32:13 +0600

    layout working perfectly

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+import Layout from '../components/Layout'
+export default function MyApp({ pageProps, Component }) {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  console.log(pageProps, ' ', Component)
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
